426e89c836a71d2c46b076c049f7a232821885e4
yiouli <liyiousu@gmail.com>
2023-12-04 19:52:26 -0800

    initial landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
   Toolbar,
   Typography,
   createTheme,
+  useTheme,
 } from '@mui/material';
 import Image from 'next/image';
 import React from 'react';
@@ -30,38 +31,104 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    // other typography settings
+    fontFamily: '"Lora", serif',
+    //fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    button: {
+      textTransform: 'none',
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderWidth: '2px', // Set border thickness here
+          borderRadius: '30px', // Set border radius here
+          fontWeight: 'bold',
+        },
+      },
+    },
   },
 }
 )
 
-export default function Home(): React.ReactElement {
-  return <ThemeProvider theme={theme}>
+export function Hero(props: {
+  url: string,
+  isBackground?: boolean,
+  backgroundShadeColor?: string,
+}): React.ReactElement {
+  const theme = useTheme();
+
+  // Utility function to convert hex code to RGB
+  const colorToRgb = (hex: string) => {
+    const bigint = parseInt(hex.replace('#', ''), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return `${r}, ${g}, ${b}`;
+  };
+
+  const backgroundShadeColor = props.backgroundShadeColor || theme.palette.background.default;
+  const backgroundShadeHex = backgroundShadeColor;
+
+  return props.isBackground
+    ? <>
+      <Box sx={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+        backgroundImage: `url(${props.url})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundColor: `rgba(${colorToRgb(backgroundShadeHex)}, 0.5)`,
+        backgroundBlendMode: 'multiply',
+      }} />
+    </>
+    : <Box
+      component='img'
+      src={props.url}
+      sx={{ width: '100%' }}
+    />;
+}
+
+
+function HomeUI(): React.ReactElement {
+  const theme = useTheme()
+  return <>
     <AppBar
       elevation={0}
       position='fixed'
       color='transparent'
     >
-      <Toolbar>
-        <Image src='icon.png' alt='Dragonfruit Joy' width={48} height={48} />
-        <Typography variant='h3'>DragonFruitJoy</Typography>
-        <Button variant='contained' color='secondary'>What</Button>
+      <Toolbar sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}>
+        <Stack direction='row' spacing={1} display='flex' alignItems='center'>
+          <Image src='icon.png' alt='Dragonfruit Joy' width={48} height={48} />
+          <Typography variant='h4' color={theme.palette.secondary.light}>
+            <b>Dragonfruit Joy</b>
+          </Typography>
+        </Stack>
+        <Button variant='outlined' color='secondary' size='large'>Contact Us</Button>
       </Toolbar>
     </AppBar>
     <Stack spacing={4}>
-      <Box sx={{
-        height: '300px',
-        display: 'flex',
-        alignItems: 'center',
-        backgroundImage: 'url(cover.jpg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'top',
-      }}>
-        <Typography>Welcome sdfdfdfsdf</Typography>
+      <Stack
+        spacing={2}
+        height='100vh'
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        color={theme.palette.info.light}
+      >
+        <Hero url="cover.jpg" isBackground backgroundShadeColor={theme.palette.primary.dark} />
+        <Typography variant='h2'>Food, Mind, Body</Typography>
+        <Typography variant='h3'>Holistic Wellbeing</Typography>
+      </Stack>
 
-      </Box>
       <Typography>sdlfjsdlflsdkjflsdkjfsdljfdlsfjkl</Typography>
       <Typography>sdlfjsdlflsdkjflsdkjfsdljfdlsfjkl</Typography>
       <Typography>sdlfjsdlflsdkjflsdkjfsdljfdlsfjkl</Typography>
@@ -70,5 +137,12 @@ export default function Home(): React.ReactElement {
       <Typography>sdlfjsdlflsdkjflsdkjfsdljfdlsfjkl</Typography>
       <Typography>sdlfjsdlflsdkjflsdkjfsdljfdlsfjkl</Typography>
     </Stack>
-  </ThemeProvider>
+  </>
+}
+
+
+export default function Home(): React.ReactElement {
+  return <ThemeProvider theme={theme}>
+    <HomeUI />
+  </ThemeProvider >
 }
---
+++ docs/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1nITf+LWQfkeowvl4WpvDppzUMw+Bkh8HlEth/na7NE=\"\n}"
