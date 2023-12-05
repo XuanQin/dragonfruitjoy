625a0f0d26e83451dc7420fe96f8428266aa65ac
yiouli <liyiousu@gmail.com>
2023-12-04 17:51:30 -0800

    test github page settings

---
+++ docs/_next/static/chunks/app/page-bc8d6266d5fcb2fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7410:function(n,e,u){Promise.resolve().then(u.bind(u,5532))}},function(n){n.O(0,[532,971,472,744],function(){return n(n.s=7410)}),_N_E=n.O()}]);
